ddc5693223a2d538455848b06d1e39f49dae7af8
hansrajathole <hansrajathole1@gmail.com>
2025-05-22 17:55:42 +0530

    add BASE_URL configuration to use environment variable

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -5,7 +5,8 @@ dotenv.config();
 const _config ={
     PORT: process.env.PORT || 5000,
     MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017/movieData",
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
+    BASE_URL: process.env.BASE_URL || 'http://localhost:5000',
 }
 
 const config = Object.freeze(_config);
