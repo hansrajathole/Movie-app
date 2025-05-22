7870843f3ac56993c6ee93c48dff7d624ac16c2f
hansrajathole <hansrajathole1@gmail.com>
2025-05-22 18:11:51 +0530

    update BASE_URL in config and adjust API_URL in frontend to ensure consistent environment variable usage

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -6,7 +6,7 @@ const _config ={
     PORT: process.env.PORT || 5000,
     MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017/movieData",
     JWT_SECRET: process.env.JWT_SECRET,
-    BASE_URL: process.env.BASE_URL || 'http://localhost:5000',
+    BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
 }
 
 const config = Object.freeze(_config);
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:8000/api';
-
+const API_URL = process.env.REACT_APP_API_URL  || 'http://localhost:8000/api';
+console.log('API URL:', API_URL);
 const api = axios.create({
   baseURL: API_URL,
   headers: {
